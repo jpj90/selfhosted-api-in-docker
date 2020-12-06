fb1232ba8cdf06700a4ee7ea611f7fd34270154d
James Janse <james.janse@hypoport.com>
2020-12-06 22:16:44 +0100

    Add project files.

---
+++ console_try/Startup1.cs
@@ -0,0 +1,54 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace console_try
+{
+  public class Startup1
+  {
+    public Startup1(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.MapWhen(context => context.Request.Path.StartsWithSegments("/docker"), a =>
+      {
+        a.Run(async (context) =>
+        {
+          await context.Response.WriteAsync("<html><head><title>DockerHello</title></head><body><h2>Hello from Web API in Docker!</h2></body></html>");
+        });
+      });
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
